26cd34f3ece5934b9bd4f0d9b45eaae429dd8d84
J. Roberto <jose.gonzalez@gonet.us>
2019-03-08 10:18:22 -0600

    Primer Commit, Button Action, número random.

--- GAnalytics/ViewController.swift
+++ GAnalytics/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numeroLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func numeroButtonAction(_ sender: Any) {
+        
+        let number = Int.random(in: 0 ... 1000)
+        numeroLabel.text = String(number)
+    }
+    
 
 }
 
